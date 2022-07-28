81c3e18a0d0bde0932419b824f104c428c50d994
Michael Buffone <m_buffone@hotmail.com>
2022-07-28 07:39:51 -0400

    Added started file for server install some dependencies

---
+++ server/server.js
@@ -0,0 +1,13 @@
+// example to test if server is working
+
+const express = require("express");
+const app = express();
+const port = 3001;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
