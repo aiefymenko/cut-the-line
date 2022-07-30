6a61c95205b27228ade73d4210c7769121f51a0e
Artem Iefymenko <iefymenko.artem@gmail.com>
2022-07-30 18:40:41 -0400

    Add /user form and popup user registration on admin

---
+++ client/src/components/admin/FeatureSection/Buttons/Buttons.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button'
+import './Buttons.scss'
+
+export default function Buttons() {
+  return (
+    <Button as="input" type="submit" value="Submit" />
+  )
+}
--- client/src/components/user/UserApp.js
+++ client/src/components/user/UserApp.js
@@ -1,12 +1,14 @@
-import React from "react";
-import User from "./User";
+import React from 'react'
+import Buttons from '../admin/FeatureSection/Buttons/Buttons'
+import CreateUser from '../admin/FeatureSection/UserDetails/CreateUser'
+import './UserApp.scss'
 
-const UserApp = () => {
+export default function UserApp () {
   return (
-    <div>
-      <User />
+    <div className ='userForm'>
+      <h1> Enter your details</h1>
+    <CreateUser />
+    <Buttons />
     </div>
-  );
-};
-
-export default UserApp;
+  )
+}
