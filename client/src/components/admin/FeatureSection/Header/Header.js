c630c9cddc5476d6c4e60a3e4a8dd94e107c09bd
Artem Iefymenko <iefymenko.artem@gmail.com>
2022-07-30 18:40:41 -0400

    Add /user form and popup user registration on admin

--- client/src/components/admin/FeatureSection/Header/Header.js
+++ client/src/components/admin/FeatureSection/Header/Header.js
@@ -1,18 +1,27 @@
 import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import logo from "../../../../images/logo.jpeg";
+import Popup from "../UserDetails/Popup";
 
 import "./Header.scss";
 
 const Header = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
+    <>
     <div className="header">
       <img src={logo} />
       <h1>Cut The Line</h1>
-      <Button className="create" variant="primary">
+      <Button className="create" variant="primary" onClick={handleShow}>
         Add Client
       </Button>
     </div>
+    {show && <Popup handleClose={handleClose} /> }
+    </>
   );
 };
 
