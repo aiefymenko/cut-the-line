ed6921cd28bfdc5dc203cb40202273bdace96181
Joe Tang <joetang81@gmail.com>
2022-07-31 18:29:09 -0400

    Implement all server side routes except new_session.js

---
+++ server/routes/move_lower_position.js
@@ -0,0 +1,54 @@
+const router = require("express").Router();
+
+module.exports = db => {
+  router.post("/move_lower_position/:session_id", (request, response) => {
+    const queryString =
+      `
+    UPDATE sessions
+    SET position = position - 1
+    WHERE position > $1 AND position <= $2
+    `;
+
+    const values = [request.body.old_position, request.body.new_position];
+
+    db.query(queryString, values)
+      .then(() => {
+        response.status(204).json({});
+      })
+      .catch(err => {
+        response
+          .status(500)
+          .json({ error: err.message });
+      });
+
+    const queryString1 =
+      `
+      UPDATE sessions
+      SET position = $1
+      WHERE id = $2
+    `;
+
+    const values1 = [request.body.new_position, request.params.session_id];
+
+    db.query(queryString1, values1)
+      .then(() => {
+        response.status(204).json({});
+
+      })
+      .catch(err => {
+        response
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  return router;
+};
+
+// request call:
+// POST - localhost:3001/api/move_lower_position/2
+// JSON Body:
+// {
+//   "old_position": 2,
+//   "new_position": 4
+// }
