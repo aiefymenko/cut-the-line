0759ef07afc3eacedfe3d867992bf7ccc7ebdf68
Michael Buffone <m_buffone@hotmail.com>
2022-08-03 06:35:27 -0400

    Started adding twilio not close to complete

--- server/server.js
+++ server/server.js
@@ -4,9 +4,8 @@ require("dotenv").config();
 const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const port = 3001;
-
 
 //Connect our Database to server
 const { Pool } = require("pg");
@@ -18,6 +17,21 @@ db.connect();
 app.use(bodyParser.json());
 app.use(cors());
 
+//twilio
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Find your Account SID and Auth Token at twilio.com/console
+// and set the environment variables. See http://twil.io/secure
+// const accountSid = process.env.TWILIO_ACCOUNT_SID;
+// const authToken = process.env.TWILIO_AUTH_TOKEN;
+// const client = require("twilio")(accountSid, authToken);
+
+// client.messages
+//   .create({
+//     body: "Your on the waitlist",
+//     from: "+19206146871",
+//     to: "+15145746006",
+//   })
+//   .then((message) => console.log(message.status));
 
 const complete_session = require("./routes/complete_session");
 const edit_user = require("./routes/edit_user");
@@ -27,7 +41,6 @@ const move_lower_position = require("./routes/move_lower_position");
 const new_session = require("./routes/new_session");
 const get_settings = require("./routes/get_settings");
 
-
 app.use("/api", complete_session(db));
 app.use("/api", edit_user(db));
 app.use("/api", get_sessions(db));
@@ -35,7 +48,6 @@ app.use("/api", move_upper_position(db));
 app.use("/api", move_lower_position(db));
 app.use("/api", new_session(db));
 app.use("/api", get_settings(db));
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
