fa0036972166ca8b59d587be2513d8dc7ea61095
Michael Buffone <m_buffone@hotmail.com>
2022-08-03 19:02:45 -0400

    Refactor twilio into routes and hopefully gitignore works on .env file

---
+++ server/routes/twilio.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+require("dotenv").config();
+
+module.exports = () => {
+  const accountSid = process.env.TWILIO_ACCOUNT_SID;
+  const authToken = process.env.TWILIO_AUTH_TOKEN;
+  const TWILIO_PHONE_NUMBER = process.env.TWILIO_PHONE_NUMBER;
+  const client = require("twilio")(accountSid, authToken, TWILIO_PHONE_NUMBER);
+  //twilio
+  router.post("/messages", (req, res) => {
+    console.log(req.body);
+    client.messages
+      .create({
+        from: process.env.TWILIO_PHONE_NUMBER,
+        to: req.body.to,
+        body: req.body.body,
+      })
+      .then(() => {
+        res.status(201).send(req.body.body);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+  return router;
+};
--- server/server.js
+++ server/server.js
@@ -19,41 +19,6 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(pino);
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const TWILIO_PHONE_NUMBER = process.env.TWILIO_PHONE_NUMBER;
-const client = require("twilio")(accountSid, authToken, TWILIO_PHONE_NUMBER);
-//twilio
-app.post("/api/messages", (req, res) => {
-  // res.header("Content-Type", "application/json");
-  console.log(req.body);
-  client.messages
-    .create({
-      from: process.env.TWILIO_PHONE_NUMBER,
-      to: req.body.to,
-      body: req.body.body,
-    })
-    .then(() => {
-      res.status(201).send(req.body.body);
-    })
-    .catch((err) => {
-      res.status(500).json({ error: err.message });
-    });
-});
-
-// client.messages
-// .create({
-//   from: process.env.TWILIO_PHONE_NUMBER,
-//   to: req.body.to,
-//   body: req.body.body,
-// })
-// .then(() => {
-// res.status(201).send();
-// })
-// .catch((err) => {
-//   res.status(500).json({ error: err.message });
-// });
-
 const complete_session = require("./routes/complete_session");
 const edit_user = require("./routes/edit_user");
 const get_sessions = require("./routes/get_sessions");
@@ -61,6 +26,7 @@ const move_upper_position = require("./routes/move_upper_position");
 const move_lower_position = require("./routes/move_lower_position");
 const new_session = require("./routes/new_session");
 const get_settings = require("./routes/get_settings");
+const twilio = require("./routes/twilio");
 
 app.use("/api", complete_session(db));
 app.use("/api", edit_user(db));
@@ -69,6 +35,7 @@ app.use("/api", move_upper_position(db));
 app.use("/api", move_lower_position(db));
 app.use("/api", new_session(db));
 app.use("/api", get_settings(db));
+app.use("/api", twilio());
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
